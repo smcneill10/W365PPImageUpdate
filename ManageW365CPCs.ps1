0cce5b148c73b8a89a54ad145a0b48c70dd5eb01
Sean McNeill <sean.mcneill@outlook.com>
2024-06-22 14:53:00 -0600

    revert to Beta

--- ManageW365CPCs.ps1
+++ ManageW365CPCs.ps1
@@ -219,7 +219,7 @@ Function Get-ProvisionPolicyInfo
 #Connect to CloudPC Graph API 
 Connect-MgGraph -Scopes "CloudPC.ReadWrite.All, User.Read.All","Group.Read.All, CloudPC.read.all"
 #Set Graph API to Beta
-#Select-MgProfile Beta
+Select-MgProfile Beta
 
 #Gathers the connection info, comment out the Clear-Host line below to see this info, helps with connectivity issues
 Write-host "Here is the connection information used:" -BackgroundColor $BKColorInfo -ForegroundColor $FGColor
